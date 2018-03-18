8c5016587e68024a61ffd144df44086210d900ac
FreakingBarbarians <Paarthurnax@live.ca>
2018-03-17 22:00:17 -0400

    added circular targets

--- visualization/sankey.js
+++ visualization/sankey.js
@@ -55,12 +55,12 @@ d3.sankey = function() {
  
     function link(d) {
       var x0 = d.source.x + d.source.dx,
-          x1 = d.target.x,
+          x1 = d.target.x + Math.random()*30,
           xi = d3.interpolateNumber(x0, x1),
           x2 = xi(curvature),
           x3 = xi(1 - curvature),
           y0 = d.source.y + d.sy + d.dy / 2,
-          y1 = d.target.y + d.ty + d.dy / 2;
+          y1 = d.target.y + Math.random()*30;
       return "M" + x0 + "," + y0
            + "C" + x2 + "," + y0
            + " " + x3 + "," + y1
